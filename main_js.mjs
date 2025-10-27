2247297928318334ca59478f81613071451ed82c
M N Tushar <M N Tushar@DESKTOP-56P878A>
2025-10-27 11:24:23 +0600

    add example

--- main_js.mjs
+++ main_js.mjs
@@ -17,16 +17,14 @@ function monitorMainThread(interval = 100) {
 monitorMainThread();
 
 (async () => {
+  // example for file
   let resultAdd;
-  // const task = { a: 5, b: 5 };
-  // resultAdd = await pool.runTaskScriptPath('./addition.js', { iterations: 5_000_000 });
-  // console.log(resultAdd);
+  const task = { a: 5, b: 5 };
+  resultAdd = await pool.runTaskScriptPath('./addition.js', task);
+  console.log(resultAdd);
 
-  // const resultMulti = await pool.runTaskScriptPath('./multi.js', task);
-  // console.log(resultMulti);
 
-  // await pool.runTaskScriptPath('./invoice_pdf_run.mjs', null);
-
+  // example for code excute
   const codef = (task) => {
     const results = [];
 
@@ -44,12 +42,32 @@ monitorMainThread();
 
     return results.length;
   };
-
-  const codes = `
-  const fn = ${codef.toString()};
-  return fn(task);
-`;
-
+  const codes = pool.functionToString(codef, codef.name);
   resultAdd = await pool.runTaskScriptCode(codes, { iterations: 5_000_000 });
+  console.log(resultAdd);
+
+  // example for code excute with dependency
+  const codefd = (data, dependencies) => {
+    const results = [];
+
+    function isPrime(n) {
+      if (n < 2) return false;
+      for (let i = 2; i * i <= n; i++) {
+        if (n % i === 0) return false;
+      }
+      return true;
+    }
+
+    for (let i = 2; i < task.iterations; i++) {
+      if (isPrime(i)) results.push(i);
+    }
+
+    const aTask = { a: results.length, b: 5 };
+    return dependencies['addition'].default(aTask);
+  };
+  const codesd = pool.functionToString(codefd, codefd.name);
+  const iterations = 5_000_000;
+  const data = { iterations, dependencyPaths: ['./addition.js'] };
+  resultAdd = await pool.runTaskScriptCode(codesd, data);
   console.log(resultAdd);
 })();
