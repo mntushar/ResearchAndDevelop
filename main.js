5a54874014f2b18a3b9b5c9b043f9972d6947e8f
mntushar <mntushar25@gmail.com>
2025-12-09 15:35:17 +0600

    run code force code

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
+const os = require('os');
 
-const [input] = fs.readFileSync(0, 'utf8').trim();
+const [, input] = fs.readFileSync(0, 'utf8').trim().split(os.EOL);
 
+const result = input.split('').reduce((count, currentValue, index, array) => {
+    if(index > 0 && currentValue === array[index - 1])
+        count += 1;
+    return count;
+}, 0);
 
-console.log(input);
+console.log(result);
--- main.mjs
+++
@@ -1,6 +0,0 @@
-import { readFileSync } from 'fs';
-
-const [input] = readFileSync(0, 'utf8').trim();
-
-
-console.log(input);
