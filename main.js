542e89b0a96e72e5c55a4a713db21eaea50b5b2f
mntushar <mntushar25@gmail.com>
2026-04-28 22:33:40 +0600

    remove ts

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-const fs = require('fs');
+const fs = require("fs");
 const os = require('os');
 
-const [, input] = fs.readFileSync(0, 'utf8').trim().split(os.EOL);
+const input = fs.readFileSync(0, "utf8").trim().split(/\s+/).map(Number);
+let idx = 0;
 
-const result = input.split('').reduce((count, currentValue, index, array) => {
-    if(index > 0 && currentValue === array[index - 1])
-        count += 1;
-    return count;
-}, 0);
+const MOD = 676767677;
+const t = input[idx++];
+let ans = [];
 
-console.log(result);
+for (let tc = 0; tc < t; tc++) {
+  const n = input[idx++];
+  let a = [];
+  let sumGreater = 0;
+  let hasGreater = false;
+
+  for (let i = 0; i < n; i++) {
+    a.push(input[idx++]);
+    if (a[i] > 1) {
+      sumGreater += a[i];
+      hasGreater = true;
+    }
+  }
+
+  let res;
+  if (!hasGreater) {
+    res = 1;
+  } else {
+    res = sumGreater;
+    if (a[n - 1] === 1) res += 1;
+  }
+
+  ans.push(String(res % MOD));
+}
+
+console.log(ans.join("\n"));
